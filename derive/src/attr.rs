a0393dab12ff3cc09f8e4572b864c51f16ea6661
David Tolnay <dtolnay@gmail.com>
2019-08-17 19:31:15 -0700

    Update to syn 1.0

--- derive/src/attr.rs
+++ derive/src/attr.rs
@@ -9,14 +9,14 @@ pub fn name_of_field(field: &Field) -> String {
             continue;
         }
 
-        let list = match attr.interpret_meta() {
-            Some(Meta::List(list)) => list,
+        let list = match attr.parse_meta() {
+            Ok(Meta::List(list)) => list,
             _ => panic!("unsupported attribute"),
         };
 
         for meta in list.nested {
             if let NestedMeta::Meta(Meta::NameValue(value)) = meta {
-                if value.ident == "rename" && rename.is_none() {
+                if value.path.is_ident("rename") && rename.is_none() {
                     if let Lit::Str(s) = value.lit {
                         rename = Some(s.value());
                         continue;
