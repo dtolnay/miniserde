b8c93a3f6e41c2e2b932f12303678cda521dae91
David Tolnay <dtolnay@gmail.com>
2019-08-25 12:47:09 -0700

    Rename find_rename_attr -> attr_rename

--- derive/src/attr.rs
+++ derive/src/attr.rs
@@ -1,7 +1,7 @@
 use syn::{Attribute, Error, Field, Lit, Meta, NestedMeta, Result, Variant};
 
-/// Find the value of a #[serde(rename = "xxx")] attribute.
-fn find_rename_attr(attrs: &[Attribute]) -> Result<Option<String>> {
+/// Find the value of a #[serde(rename = "...")] attribute.
+fn attr_rename(attrs: &[Attribute]) -> Result<Option<String>> {
     for attr in attrs {
         if !attr.path.is_ident("serde") {
             continue;
@@ -28,12 +28,12 @@ fn find_rename_attr(attrs: &[Attribute]) -> Result<Option<String>> {
 
 /// Determine the name of a field, respecting a rename attribute.
 pub fn name_of_field(field: &Field) -> Result<String> {
-    let rename = find_rename_attr(&field.attrs)?;
+    let rename = attr_rename(&field.attrs)?;
     Ok(rename.unwrap_or_else(|| field.ident.as_ref().unwrap().to_string()))
 }
 
 /// Determine the name of a variant, respecting a rename attribute.
 pub fn name_of_variant(var: &Variant) -> Result<String> {
-    let rename = find_rename_attr(&var.attrs)?;
+    let rename = attr_rename(&var.attrs)?;
     Ok(rename.unwrap_or_else(|| var.ident.to_string()))
 }
