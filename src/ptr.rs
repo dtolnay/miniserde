ea42df3c6251f3c5d4de8b589fa2aa27d728efa2
David Tolnay <dtolnay@gmail.com>
2021-12-31 11:44:40 -0800

    Support ?Sized values in NonuniqueBox

--- src/ptr.rs
+++ src/ptr.rs
@@ -7,32 +7,38 @@ use core::ptr::NonNull;
 // makes it possible to take a &mut reference to the heap allocation, then move
 // the NonuniqueBox, then write to the heap allocation through that old
 // reference, then drop the NonuniqueBox.
-pub struct NonuniqueBox<T> {
+pub struct NonuniqueBox<T: ?Sized> {
     ptr: NonNull<T>,
 }
 
 impl<T> NonuniqueBox<T> {
     pub fn new(value: T) -> Self {
-        let ptr = Box::into_raw(Box::new(value));
+        NonuniqueBox::from(Box::new(value))
+    }
+}
+
+impl<T: ?Sized> From<Box<T>> for NonuniqueBox<T> {
+    fn from(boxed: Box<T>) -> Self {
+        let ptr = Box::into_raw(boxed);
         let ptr = unsafe { NonNull::new_unchecked(ptr) };
         NonuniqueBox { ptr }
     }
 }
 
-impl<T> Deref for NonuniqueBox<T> {
+impl<T: ?Sized> Deref for NonuniqueBox<T> {
     type Target = T;
     fn deref(&self) -> &Self::Target {
         unsafe { self.ptr.as_ref() }
     }
 }
 
-impl<T> DerefMut for NonuniqueBox<T> {
+impl<T: ?Sized> DerefMut for NonuniqueBox<T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         unsafe { self.ptr.as_mut() }
     }
 }
 
-impl<T> Drop for NonuniqueBox<T> {
+impl<T: ?Sized> Drop for NonuniqueBox<T> {
     fn drop(&mut self) {
         let ptr = self.ptr.as_ptr();
         let _ = unsafe { Box::from_raw(ptr) };
