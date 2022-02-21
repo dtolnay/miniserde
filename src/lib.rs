b5798fd874a7ad06ec09abbe1bca2decc3547d4e
David Tolnay <dtolnay@gmail.com>
2022-02-21 13:27:23 -0800

    Release 0.1.22

--- src/lib.rs
+++ src/lib.rs
@@ -133,7 +133,7 @@
 //! restricts the kinds of on-the-fly manipulation that are possible in custom
 //! impls. If you need any of this, use Serde -- it's a great library.
 
-#![doc(html_root_url = "https://docs.rs/miniserde/0.1.21")]
+#![doc(html_root_url = "https://docs.rs/miniserde/0.1.22")]
 #![allow(
     clippy::needless_doctest_main,
     clippy::vec_init_then_push,
