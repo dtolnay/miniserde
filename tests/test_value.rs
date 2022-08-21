cfaab592316082ff989c2cf9924915f6ef473693
David Tolnay <dtolnay@gmail.com>
2022-08-20 21:21:58 -0700

    Add test of Value's Debug representation

--- tests/test_value.rs
+++ tests/test_value.rs
@@ -1,3 +1,4 @@
+use indoc::indoc;
 use miniserde::json::{self, Value};
 
 #[test]
@@ -15,4 +16,64 @@ fn test_round_trip_deeply_nested() {
     let value: Value = json::from_str(&j).unwrap();
     let j2 = json::to_string(&value);
     assert_eq!(j, j2);
+}
+
+#[test]
+fn test_debug() {
+    let j = r#"
+        {
+            "Null": null,
+            "Bool": true,
+            "Number": 1,
+            "String": "...",
+            "Array": [true],
+            "EmptyArray": [],
+            "EmptyObject": {}
+        }
+    "#;
+
+    let value: Value = json::from_str(&j).unwrap();
+    let debug = format!("{:#?}", value);
+
+    let expected = indoc! {r#"
+        Object(
+            Object {
+                inner: {
+                    "Array": Array(
+                        Array {
+                            inner: [
+                                Bool(
+                                    true,
+                                ),
+                            ],
+                        },
+                    ),
+                    "Bool": Bool(
+                        true,
+                    ),
+                    "EmptyArray": Array(
+                        Array {
+                            inner: [],
+                        },
+                    ),
+                    "EmptyObject": Object(
+                        Object {
+                            inner: {},
+                        },
+                    ),
+                    "Null": Null,
+                    "Number": Number(
+                        U64(
+                            1,
+                        ),
+                    ),
+                    "String": String(
+                        "...",
+                    ),
+                },
+            },
+        )"#
+    };
+
+    assert_eq!(debug, expected);
 }
