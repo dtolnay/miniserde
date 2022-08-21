4b021e8663326bc7df2b39ab87f0ce619e785f90
David Tolnay <dtolnay@gmail.com>
2022-08-20 21:56:43 -0700

    Implement Deserialize for Number

--- src/json/number.rs
+++ src/json/number.rs
@@ -1,4 +1,7 @@
+use crate::de::{Deserialize, Visitor};
+use crate::error::Result;
 use crate::ser::{Fragment, Serialize};
+use crate::Place;
 
 /// A JSON number represented by some Rust primitive.
 #[derive(Clone, Debug)]
@@ -15,5 +18,28 @@ impl Serialize for Number {
             Number::I64(n) => Fragment::I64(*n),
             Number::F64(n) => Fragment::F64(*n),
         }
+    }
+}
+
+impl Deserialize for Number {
+    fn begin(out: &mut Option<Self>) -> &mut dyn Visitor {
+        impl Visitor for Place<Number> {
+            fn negative(&mut self, n: i64) -> Result<()> {
+                self.out = Some(Number::I64(n));
+                Ok(())
+            }
+
+            fn nonnegative(&mut self, n: u64) -> Result<()> {
+                self.out = Some(Number::U64(n));
+                Ok(())
+            }
+
+            fn float(&mut self, n: f64) -> Result<()> {
+                self.out = Some(Number::F64(n));
+                Ok(())
+            }
+        }
+
+        Place::new(out)
     }
 }
