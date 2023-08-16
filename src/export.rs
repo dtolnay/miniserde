5da07578a843c887f07e537deb6d72d0c9d43127
David Tolnay <dtolnay@gmail.com>
2023-08-16 16:09:04 -0700

    Switch to using primitives in macro-generated code through core::primitive

--- src/export.rs
+++ src/export.rs
@@ -2,12 +2,5 @@ pub use alloc::borrow::Cow;
 pub use alloc::boxed::Box;
 pub use alloc::string::String;
 pub use core::option::Option::{self, None, Some};
+pub use core::primitive::{str, usize};
 pub use core::result::Result::{Err, Ok};
-
-pub use self::help::Str as str;
-pub use self::help::Usize as usize;
-
-mod help {
-    pub type Str = str;
-    pub type Usize = usize;
-}
