0240792fd2d4e56a17c3811e6eda5e4faf8062af
David Tolnay <dtolnay@gmail.com>
2024-02-18 20:26:46 -0800

    Release 0.1.37

--- src/lib.rs
+++ src/lib.rs
@@ -134,7 +134,7 @@
 //! restricts the kinds of on-the-fly manipulation that are possible in custom
 //! impls. If you need any of this, use Serde -- it's a great library.
 
-#![doc(html_root_url = "https://docs.rs/miniserde/0.1.36")]
+#![doc(html_root_url = "https://docs.rs/miniserde/0.1.37")]
 #![allow(
     clippy::cast_possible_wrap,
     clippy::cast_precision_loss,
